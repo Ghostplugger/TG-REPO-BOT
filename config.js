4bf6bcf832cf19d534a37373d562f98ed02d119f
Ghostplugger <kanhapandit143x@gmail.com>
2026-08-17 22:19:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const settings = {
+  token: '8762591813:AAF9EQcR1BKvymM96iNjfXJ4YjoitrscZUU',
+  ownerId: 7380059056,
+  dev: 'sneuo',
+  
+  panel: 'https://files.catbox.moe/9urasn.jpg',
+};
+
+module.exports = settings;
