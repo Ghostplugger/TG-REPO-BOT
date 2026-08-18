9c7b3e596e81d85f70cf0fe0087de64aa73820bc
Ghostplugger <kanhapandit143x@gmail.com>
2026-08-18 09:57:54 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const settings = {
-  token: '8762591813:AAF9EQcR1BKvymM96iNjfXJ4YjoitrscZUU',
-  ownerId: 7380059056,
-  dev: 'sneuo',
-  
-  panel: 'https://files.catbox.moe/9urasn.jpg',
-};
-
-module.exports = settings;
